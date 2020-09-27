61916a7204d6df20535097bfcbf0cf07108b5902
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2020-09-26 20:59:03 -0500

    Add initial files.

---
+++ src/test/java/com/csoftz/region/municipality/MunicipalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csoftz.region.municipality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicipalityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
