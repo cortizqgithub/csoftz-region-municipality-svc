5f826acd67400d009d93db97bc51a89065013fb9
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2020-12-14 10:21:32 -0500

    Change POM.XML format and upgrade to SB 2.4.1
    Format code files.

--- src/test/java/com/csoftz/region/municipality/MunicipalityApplicationTests.java
+++ src/test/java/com/csoftz/region/municipality/MunicipalityApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MunicipalityApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
